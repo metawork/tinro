d85ff664ed11108ff904300209a0157eff78b6f1
willtachau <will@metawork.com>
2021-02-25 04:00:03 +0000

    log

--- src/location.js
+++ src/location.js
@@ -46,6 +46,7 @@ function createLocation(){
 }
 
 function setLocation(MODE,href){
+    console.log("SET LOCATION");
     MODES.run( MODE,
         _ => history.pushState({}, '', href),
         _ => window.location.hash=href,
